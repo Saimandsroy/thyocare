6a156f748f9c2fc070b0c2b5d03cdd2b72f4a645
Saimands Roy <saimandsroy@Saimandss-MacBook-Air.local>
2025-12-22 10:01:16 +0530

    Configure for Cloudflare Pages static export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'web-assets.thyrocare.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
         pathname: '/**',
       },
     ],
